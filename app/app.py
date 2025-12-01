3fa1e88e821deb0bcf68a82d30842e4be91bd4d6
JavierCuellar5 <javiercueeellar@gmail.com>
2025-12-01 10:17:08 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -39,9 +39,9 @@ if imagen_camara is not None:
     imagen = ImageOps.fit(imagen, (224, 224), Image.Resampling.LANCZOS)
     imagen_array = np.asarray(imagen)
     normalizada_imagen_array = (imagen_array.astype(np.float32) / 127.5) - 1
-# Crear un array para un lote de 1 imagen. ndarray = N-Dimensional Array
-lote_imagenes = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-lote_imagenes[0] = normalizada_imagen_array
+    # Crear un array para un lote de 1 imagen. ndarray = N-Dimensional Array
+    lote_imagenes = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    lote_imagenes[0] = normalizada_imagen_array
 
 # Predicción
 resultados= mi_modelo.predict(lote_imagenes)
